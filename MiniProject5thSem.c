8acf67b57d90f872b9f25c3b1a55c679a61aa25c
SHUBHAM RASTOGI <shubhamrastogi2895@gmail.com>
2015-11-18 15:39:54 +0530

    Update MiniProject5thSem.c

--- MiniProject5thSem.c
+++ MiniProject5thSem.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
+typedef struct segregate
+{
+    char str[10];
+}seg;
 void main()
 {
     FILE *fp,*f;
-    int i,j,address;
-    char c,c2,ch;
+    seg add[20],label[20],mnemonik[20],operand[20];
+    int i,j,address,o=0,l=0,m=0,r,ctr;
+    int adres[20],length;
+    char c,c2,c3,ch;
     fp=fopen("mnemonics.txt","r");
     printf("------------------MNEMONICS THAT CAN BE USED FOR THE MACHINE:---------------\n\n");
     while(ch != EOF)
@@ -22,9 +28,33 @@ void main()
         putchar(c);
     }
     fclose(f);
-    printf("\n\n-----------------ENTER THE STARTING ADDRESS OF THE CODE:\n------------");
+     f=fopen("assemblycode.txt","r");
+    i=1;
+    while(c3 != EOF)
+    {
+      r=i%3;
+      switch(r)
+      {
+        case 0:fscanf(f,"%s",&operand[o].str);
+               o++;
+               break;
+
+        case 1: fscanf(f,"%s",&label[l].str);
+                l++;
+                break;
+
+        case 2:fscanf(f,"%s",&mnemonik[m].str);
+               m++;
+               break;
+      }
+      i++;
+      c3=getc(f);
+    }
+    fclose(f);
+    i=0;
+    printf("\n\n-----------------ENTER THE STARTING ADDRESS OF THE CODE:------------\n");
     scanf("%d",&address);
-    printf("\n\nAFTER ADDRESSING THE CODE IS:\n\n");
+    printf("\n\n------------------AFTER ADDRESSING THE CODE IS:-------------------\n\n");
      f=fopen("assemblycode.txt","r");
     printf("ADDRESS\t");
    while(c2 != EOF)
@@ -33,11 +63,16 @@ void main()
         if(c2 == '\n')
         {
             printf("%c",c2);
-            printf("%d \t",address);
+            printf("%x \t",address);// %x IS USED FOR HEXADECIMAL
+            adres[i]=address;
             address=address+3;
+            i++;
         }
         else
             printf("%c",c2);
     }
+    ctr=i;
     fclose(f);
+    length=adres[ctr-1]-adres[0];
+    printf("\n\n\n\n--------------------------LENGTH OF THE PROGRAM:   0%x--------------------\n\n",length);
 }
